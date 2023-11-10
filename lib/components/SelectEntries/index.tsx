07bb68d07363bad2227408075648a9653e42bfe3
Kenny Lauret <kenny.lauret@outlook.fr>
2023-11-10 13:21:21 +0400

    Ajout select entries

---
+++ lib/components/SelectEntries/index.tsx
@@ -0,0 +1,16 @@
+export function SelectEntries() {
+  return (
+    <form>
+        <label htmlFor="selectEntries">
+            Show 
+            <select name="select" id="selectEntries">
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+             entries
+        </label>
+    </form>
+  )
+}

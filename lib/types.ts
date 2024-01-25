c467c33dccfdef02468bfca19aa43e7c5486772e
Kenny Lauret <kenny.lauret@outlook.fr>
2024-01-25 14:55:38 +0400

    Bump version to 0.0.7

--- lib/types.ts
+++ lib/types.ts
@@ -2,7 +2,7 @@
  * @file types.ts
  * @description Types for custom-react-table
  * @license MIT
- * @version 0.0.6
+ * @version 0.0.7
  * @since 0.0.1
  */
 import { PropsWithChildren } from "react";
